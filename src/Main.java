5aea809c59579f7e2f443e6fb0a515b27fd2da47
nigarasadova21 <nigarsdova37@gmail.com>
2024-10-26 20:27:11 +0400

    isPositive code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner sc = new Scanner (System.in)
+            int a = sc.nextInt()
+    boolean issPositive = a > 0;
+        System.out.println("isPositive");
     }
+
 }
